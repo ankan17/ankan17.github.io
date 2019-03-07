cf95115c78e9d9ef0353395efb8bdedd16428970
Ankan Poddar <friendyankan@gmail.com>
2019-03-07 17:42:05 +0530

    Update info-card header

--- src/app/info-card/info-card.component.ts
+++ src/app/info-card/info-card.component.ts
@@ -9,20 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class InfoCardComponent implements OnInit {
 
   name = 'Ankan Poddar';
-  position = 'Software Engineer & Web Developer';
+  position = 'Web Developer & Open-source enthusiast';
 
   contactInfo = [
-    { icon: 'fas fa-envelope' , text: 'friendyankan@gmail.com' },
-    { icon: 'fas fa-globe', text: 'https://ankan17.github.io' },
-    { icon: 'fab fa-github', text: 'https://github.com/ankan17' },
-    { icon: 'fas fa-phone', text: '+91 98045 17793' },
-    { icon: 'fas fa-map-marker-alt', text: 'C-35, Amarabati, Sodepur' }
+    { icon: 'fas fa-envelope' , text: 'apoddar17@gmail.com', link: 'mailto:apoddar17@gmail.com' },
+    { icon: 'fas fa-globe', text: 'https://ankan17.github.io', link: 'https://ankan17.github.io' },
+    { icon: 'fas fa-phone', text: '+91 98045 17793', link: null },
+    { icon: 'fas fa-map-marker-alt', text: 'C-35, Amarabati, Sodepur', link: null }
   ];
 
   contactLinks = [
-    { icon: 'fab fa-facebook-f', href: 'https://www.facebook.com/friendyankan', color: 'indigo' },
+    { icon: 'fab fa-facebook-f', href: 'https://www.facebook.com/ankan17', color: 'indigo' },
     { icon: 'fab fa-twitter', href: 'https://twitter.com/PoddarAnkan', color: 'blue' },
-    { icon: 'fab fa-google-plus-g', href: 'https://plus.google.com/101331497280009937677', color: 'red' },
     { icon: 'fab fa-linkedin-in', href: 'https://www.linkedin.com/in/ankan-poddar-86a6b6163', color: 'blue darken-3' },
     { icon: 'fab fa-github', href: 'https://github.com/ankan17', color: 'elegant-color' }
   ];
@@ -30,6 +28,10 @@ export class InfoCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  linkIsNull(link) {
+    return (link != null);
   }
 
 }
